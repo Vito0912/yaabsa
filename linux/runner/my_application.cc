ae9f2531b2728c9288cd0c06362a1f8e1b23a62e
Finn Dittmar <finn@dittmar-ldk.de>
2026-04-22 13:16:11 +0200

    better app bar

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -1,9 +1,7 @@
 #include "my_application.h"
 
 #include <flutter_linux/flutter_linux.h>
-#ifdef GDK_WINDOWING_X11
-#include <gdk/gdkx.h>
-#endif
+#include <glib.h>
 
 #include "flutter/generated_plugin_registrant.h"
 
@@ -20,37 +18,34 @@ static void my_application_activate(GApplication* application) {
   GtkWindow* window =
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
 
-  // Use a header bar when running in GNOME as this is the common style used
-  // by applications and is the setup most users will be using (e.g. Ubuntu
-  // desktop).
-  // If running on X and not using GNOME then just use a traditional title bar
-  // in case the window manager does more exotic layout, e.g. tiling.
-  // If running on Wayland assume the header bar will work (may need changing
-  // if future cases occur).
-  gboolean use_header_bar = TRUE;
-#ifdef GDK_WINDOWING_X11
-  GdkScreen* screen = gtk_window_get_screen(window);
-  if (GDK_IS_X11_SCREEN(screen)) {
-    const gchar* wm_name = gdk_x11_screen_get_window_manager_name(screen);
-    if (g_strcmp0(wm_name, "GNOME Shell") != 0) {
-      use_header_bar = FALSE;
-    }
-  }
-#endif
-  if (use_header_bar) {
-    GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
-    gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "yaabsa");
-    gtk_header_bar_set_show_close_button(header_bar, TRUE);
-    gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
-  } else {
-    gtk_window_set_title(window, "yaabsa");
-  }
+  gtk_window_set_title(window, "yaabsa");
 
   gtk_window_set_default_size(window, 1280, 720);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
+
+  g_autofree gchar* executable_path = g_file_read_link("/proc/self/exe", nullptr);
+  if (executable_path != nullptr) {
+    g_autofree gchar* executable_dir = g_path_get_dirname(executable_path);
+    g_autofree gchar* assets_path =
+        g_build_filename(executable_dir, "data", "flutter_assets", nullptr);
+    g_autofree gchar* icu_data_path =
+        g_build_filename(executable_dir, "data", "icudtl.dat", nullptr);
+    g_autofree gchar* aot_library_path =
+        g_build_filename(executable_dir, "lib", "libapp.so", nullptr);
+
+    if (g_file_test(assets_path, G_FILE_TEST_IS_DIR)) {
+      fl_dart_project_set_assets_path(project, g_strdup(assets_path));
+    }
+    if (g_file_test(icu_data_path, G_FILE_TEST_EXISTS)) {
+      fl_dart_project_set_icu_data_path(project, g_strdup(icu_data_path));
+    }
+    if (g_file_test(aot_library_path, G_FILE_TEST_EXISTS)) {
+      fl_dart_project_set_aot_library_path(project, g_strdup(aot_library_path));
+    }
+  }
+
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
 
   FlView* view = fl_view_new(project);
