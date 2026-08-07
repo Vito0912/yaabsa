54af84a1ac5effc7ae20877104d7c7a3b6844f18
Finn Dittmar <finn@dittmar-ldk.de>
2026-08-07 08:55:43 +0200

    remove minSdk needed for zikzak_inappwebview

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "de.vito0912.yaabsa"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 26
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
